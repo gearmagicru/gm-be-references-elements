e7e703cc1a38980c5adb781c15cde85c1422cd5a
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-12 19:37:25 +0300

    Файлы локализации

---
+++ lang/elements-ru_RU.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'Tab' => 'Вкладка',
+    'Text field' => 'Текстовое поле',
+    'Time field' => 'Поле времени',
+    'Date field' => 'Поле даты',
+    'Spinner field' => 'Поле спиннера',
+    'Numeric text field' => 'Числовое текстовое поле',
+    'File upload field' => 'Поле загруки файла',
+    'Textarea field' => 'Поле области ввода текста',
+    'Checkbox field' => 'Поле флажка',
+    'Radio field' => 'Радиополе',
+    'Hidden field' => 'Скрытое поле',
+    'Lightweight HTML Editor' => 'Легкий HTML-редактор',
+    'Display-only text field' => 'Поле вывода текста',
+];
